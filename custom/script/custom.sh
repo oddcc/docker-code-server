cb8c95633abac6a3d2624d9f4df2b3da61b85895
oddcc <oddcc@users.noreply.github.com>
2024-05-29 12:20:05 +0800

    bump all dependencies to latest

--- custom/script/custom.sh
+++ custom/script/custom.sh
@@ -1,24 +1,29 @@
-# install nvm and nodejs 18
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+# install nvm and nodejs
+NVM_VERSION=v0.39.7
+rm -rf /config/.nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
 echo 'export NVM_DIR="$HOME/.nvm"' >> /root/.bashrc
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm' >> /root/.bashrc
 . /root/.bashrc
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install 18
-nvm use 18
+nvm install --lts
+nvm use --lts
+
 # install java
 apt-get update
 apt-get install -y \
 	openjdk-8-jdk-headless \
 	openjdk-11-jdk-headless \
 	openjdk-17-jdk-headless
+
 # install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update
 apt-get install -y yarn
+
 # install python, make, g++, chromium-browser
 apt-get update
 apt-get install -y \
@@ -27,14 +32,16 @@ apt-get install -y \
 	python3 \
 	chromium-browser \
 	vim
+
 # install golang
-GOLANG_VERSION=1.21.1
+GOLANG_VERSION=1.22.3
 curl -OL https://go.dev/dl/go$GOLANG_VERSION.linux-arm64.tar.gz
 tar -C /usr/local -xf go$GOLANG_VERSION.linux-arm64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bashrc
 rm go$GOLANG_VERSION.linux-arm64.tar.gz
+
 # install maven
-MAVEN_VERSION=3.9.4
+MAVEN_VERSION=3.9.7
 curl -OL https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
 tar -xf apache-maven-$MAVEN_VERSION-bin.tar.gz
 mv apache-maven-$MAVEN_VERSION /opt/
