52cee001c8dd0888206df9c8dbbbd4fd8693668f
oddcc <oddcc@users.noreply.github.com>
2024-09-01 11:49:43 +0800

    Update custom.sh

--- custom/script/custom.sh
+++ custom/script/custom.sh
@@ -57,8 +57,10 @@ apt-get install -y \
     python3-pip
 
 # install pipenv
+echo 'installing pipenv'
 pip install pipenv
 eval "$(_PIPENV_COMPLETE=bash_source pipenv)"
+echo 'pipenv installed'
 
 # install golang
 GOLANG_VERSION=1.22.3
